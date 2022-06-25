a6b4ce2a5f4b6d798821b80c1a1592d4515cdf19
travisbrigman <travis1000@gmail.com>
2022-06-25 12:47:53 -0500

    Initial Commit

---
+++ HealthKitTest WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthKitTest WatchKit Extension
+//
+//  Created by Travis Brigman on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthKitTest WatchKit Extension/HealthKitTestApp.swift
@@ -0,0 +1,19 @@
+//
+//  HealthKitTestApp.swift
+//  HealthKitTest WatchKit Extension
+//
+//  Created by Travis Brigman on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}

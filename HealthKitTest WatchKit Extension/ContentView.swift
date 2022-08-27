75e425b95cd685408e7f512265678203185d196c
travisbrigman <travis1000@gmail.com>
2022-08-27 07:28:20 -0500

    adds fox body components

--- HealthKitTest WatchKit Extension/ContentView.swift
+++ HealthKitTest WatchKit Extension/ContentView.swift
@@ -15,10 +15,17 @@ struct ContentView: View {
     let tailColor2 = UIColor.fromHexString("#D86C13")
     let tailColor3 = UIColor.fromHexString("#FED9BE")
     let tailColor4 = UIColor.fromHexString("#F2C2A0")
+    let bodyColor1 = UIColor.fromHexString("#F88F1C")
+    let bodyColor2 = UIColor.fromHexString("#ED7E11")
+    let bodyColor3 = UIColor.fromHexString("#FED9BE")
     let feetColor1 = UIColor.fromHexString("#3F3F44")
     let feetColor2 = UIColor.fromHexString("#ED7E11")
     let feetColor3 = UIColor.fromHexString("#F88F1C")
-    let pathBounds = UIBezierPath.calculateBounds(paths: [.tailComponent1, .tailComponent2, .tailComponent3, .tailComponent4, .feet1, .feet2, .feet3])
+    let armColor1 = UIColor.fromHexString("#ED7E11")
+    let armColor2 = UIColor.fromHexString("#FFA143")
+    let rightArmcolor1 = UIColor.fromHexString("#F88F1C")
+    let rightArmColor2 = UIColor.fromHexString("#FFA143")
+    let pathBounds = UIBezierPath.calculateBounds(paths: [.tailComponent1, .tailComponent2, .tailComponent3,.tailComponent4, .bodyComponent1, .bodyComponent2, .bodyComponent3, .feetComponent1, .feetComponent2, .feetComponent3 ,.leftArmComponent1,.leftArmComponent2, .rightArmComponent1, .rightArmComponent2])
     var body: some View {
 //        let cumulativeActivityScore = viewModel.totalActivity(with: healthManager)
         let mockActivity = viewModel.mockTotalActivity()
@@ -27,15 +34,33 @@ struct ContentView: View {
         let mockGoals = viewModel.mockTotalGoals()
         let cumulativeGoalNumber = mockGoals
         ZStack {
+            Group {
             FoxHappy(bezier: .tailComponent1, pathBounds: pathBounds).fill(Color(tailColor1))
             FoxHappy(bezier: .tailComponent2, pathBounds: pathBounds).fill(Color(tailColor2))
             FoxHappy(bezier: .tailComponent3, pathBounds: pathBounds).fill(Color(tailColor3))
             FoxHappy(bezier: .tailComponent4, pathBounds: pathBounds).fill(Color(tailColor4))
-            Group {
-                FoxHappy(bezier: .feet1, pathBounds: pathBounds).fill(Color(feetColor1))
-                FoxHappy(bezier: .feet2, pathBounds: pathBounds).fill(Color(feetColor2))
-                FoxHappy(bezier: .feet3, pathBounds: pathBounds).fill(Color(feetColor3))
             }
+            
+            Group {
+            FoxHappy(bezier: .bodyComponent1, pathBounds: pathBounds).fill(Color(bodyColor1))
+            FoxHappy(bezier: .bodyComponent2, pathBounds: pathBounds).fill(Color(bodyColor2))
+            FoxHappy(bezier: .bodyComponent3, pathBounds: pathBounds).fill(Color(bodyColor3))
+            }
+
+            Group {
+            FoxHappy(bezier: .feetComponent1, pathBounds: pathBounds).fill(Color(feetColor1))
+            FoxHappy(bezier: .feetComponent2, pathBounds: pathBounds).fill(Color(feetColor2))
+            FoxHappy(bezier: .feetComponent3, pathBounds: pathBounds).fill(Color(feetColor3))
+            }
+            Group {
+                FoxHappy(bezier: .leftArmComponent1, pathBounds: pathBounds).fill(Color(armColor1))
+                FoxHappy(bezier: .leftArmComponent2, pathBounds: pathBounds).fill(Color(armColor2))
+            }
+            Group {
+                FoxHappy(bezier: .rightArmComponent1, pathBounds: pathBounds).fill(Color(rightArmcolor1))
+                FoxHappy(bezier: .rightArmComponent2, pathBounds: pathBounds).fill(Color(rightArmColor2))
+            }
+
         }
         /*
                 VStack {
